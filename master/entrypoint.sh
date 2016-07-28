8bf760811a0cc129c744cf155daaf8bb8f06b1ab
Evelyn Osman <ashex@chipnick.com>
2016-07-28 15:21:02 +0200

    Updated for v2.1.0

--- master/entrypoint.sh
+++ master/entrypoint.sh
@@ -1,133 +1,31 @@
 #!/bin/sh
 
-# Grab the arguments and store them as variables
-# If a variable and an argument were passed for the same setting
-# The argument wins
-while :
-do
-    case "$1" in
-      -a | --auth-service)
-	  AUTH_SERVICE="$2"  
-	  shift 2
-	  ;;
-      -u | --username)
-	  USERNAME="$2"  
-	  shift 2
-	  ;;
-      -p | --password)
-	  PASSWORD="$2" 
-	  shift 2
-	  ;;
-      -l | --location)
-	  LOCATION="$2"
-	  shift 2
-	  ;;
-      -st | --step-limit)
-	  STEP_LIMIT="$2"
-	  shift 2
-	  ;;
-      -i | --ignore)
-	  IGNORE="$2"
-	  shift 2
-	  ;;
-      -o | --only)
-	  ONLY="$2"
-	  shift 2
-	  ;;
-      -l | --locale)
-	  LOCALE="$2"
-	  shift 2
-	  ;;
-      -ar | --auto_refresh)
-	  AUTO_REFRESH="$2"
-	  shift 2
-	  ;;
-      -k | --google-maps-key)
-	  GMAPS_KEY="$2"
-	  shift 2
-	  ;;
-      -ns | --no-server)
-	  NO_SERVER="TRUE"
-	  shift 1
-	  ;;
-      -C | --cors)
-	  CORS="TRUE"
-	  shift 1
+# Grab the variables starting with POKEMON_
+# and create the config.ini with them
+create_config_ini () 
+{
+# Change the API keys in credentials.json 
+# If a variable with the value is provided
+    IFS=$'\n'
+    set -f
+    for VAR in `env`
+    do
+      case "$VAR" in
+          POKEMON_* )
+          #This was going to be a single sed but I couldn't figure out how to do lowercase conversion
+          key_name=`echo "$VAR" | sed -e "s/^POKEMON_\(.*\)\=.*/\1/" -e 's/_/-/g' | tr '[:upper:]' '[:lower:]'` 
+          echo "Setting value of" $key_name
+          key_value=`echo "$VAR" | sed -e "s/.*=\(.*\)/\1/"`
+          echo "$key_name: $key_value" >> config/config.ini
           ;;
-      -t | --threads)
-	  THREADS="$2"
-	  shift 2
-	  ;;
-      --) # End of all options
-	  shift
-	  break
-      ;;
-      -fl | --fixed-location)
-	  FIXED_LOCATION="TRUE"
-	  shift 1
-	  ;;
-      -sd | --scan-delay)
-      SCAN_DELAY="$2"
-      shift 2
-      ;;
-      -d | --db)
-      DATABASE="$2"
-      shift 2
-      ;;
-      -*)
-	  echo "Error: Unknown option: $1" >&2
-	  exit 1
-	  ;;
-      *)  # No more options
-	  break
-	  ;;
-    esac
-done
+        esac
+    done
+}
 
-# Set defaults for unset arguments 
-if [[ -z "$LOCALE" ]]; then
-   LOCALE="en"
- fi
-
-if [[ -z "$STEP_LIMIT" ]]; then
-   STEP_LIMIT=5
-fi
-
-# Store all arguments in a variable so we can extend it with optional ones
-# We pass location and api key separately because sh kept expanding even when wrapped in quotes
-ARGUMENTS="--auth-service $AUTH_SERVICE --username $USERNAME --password $PASSWORD --st $STEP_LIMIT --locale $LOCALE"
-
-# Add optional arguments
-if [[ -n "$IGNORE" ]]; then
-   ARGUMENTS="$ARGUMENTS --ignore $IGNORE"
-fi
-
-if [[ -n "$ONLY" ]]; then
-   ARGUMENTS="$ARGUMENTS --only $ONLY"
-fi
-
-if [[ -n "$NO_SERVER" ]]; then
-   ARGUMENTS="$ARGUMENTS --no-server"
-fi
-
-if [[ -n "$CORS" ]]; then
-   ARGUMENTS="$ARGUMENTS --cors"
-fi
-
-if [[ -n "$THREADS" ]]; then
-   ARGUMENTS="$ARGUMENTS --threads $THREADS"
-fi
-
-if [[ -n "$FIXED_LOCATION" ]]; then
-   ARGUMENTS="$ARGUMENTS --fixed-location"
-fi
-
-if [[ -n "$SCAN_DELAY" ]]; then
-   ARGUMENTS="$ARGUMENTS --scan-delay $SCAN_DELAY"
-fi
-
-if [[ -n "$DATABASE" ]]; then
-   ARGUMENTS="$ARGUMENTS --db $DATABASE"
+# Generate config.ini on first run only
+if [ ! -d "config" ]; then
+    mkdir config
+    create_config_ini
 fi
 
 # If the repo has already been created, pull the latest changes
@@ -139,7 +37,7 @@ if [ -d ".git" ]; then
     pip install -r requirements.txt
     npm update
     grunt jshint sass cssmin uglify
-    python runserver.py --host 0.0.0.0 $ARGUMENTS --location "$LOCATION" --google-maps-key "$GMAPS_KEY"
+    python runserver.py --host 0.0.0.0 "$@"
 
 else
     git init
@@ -148,7 +46,7 @@ else
     pip install -r requirements.txt
     npm install
     grunt jshint sass cssmin uglify
-    python runserver.py --host 0.0.0.0 $ARGUMENTS --location "$LOCATION" --google-maps-key "$GMAPS_KEY"
+    python runserver.py --host 0.0.0.0 "$@"
 fi
 
 
